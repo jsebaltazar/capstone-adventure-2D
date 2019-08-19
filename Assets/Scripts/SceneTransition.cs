f5e29a69e42059b920ffdb52a726f95741960a2d
Jesse <jsebaltazar@gmail.com>
2019-08-19 00:42:17 -0600

    Added SceneTransition feature with player transform update
    added hot soup sprite
    updated minotaur animations
    re-done all maps
    updated village map
    updated player UI

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public string sceneToLoad;
+    public Vector2 playerPosition;
+    public VectorValue playerLocation;
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && !other.isTrigger)
+        {
+            Debug.Log("Loading SampleScene");
+            playerLocation.initialValue = playerPosition;
+            SceneManager.LoadScene(sceneToLoad);
+
+        }
+       
+    }
+}
+ 
---
+++ Assets/Scripts/ScriptableObjects/VectorValue.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class VectorValue : ScriptableObject
+{
+    public Vector2 initialValue;
+    // Start is called before the first frame update
+ 
+}
