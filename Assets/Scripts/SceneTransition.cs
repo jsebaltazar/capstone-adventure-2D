048436449a820b210b12a29ea9ecab993df3279d
Jesse <jsebaltazar@gmail.com>
2019-08-19 01:08:33 -0600

    Added Fade effect between scene transition

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -8,6 +8,16 @@ public class SceneTransition : MonoBehaviour
     public string sceneToLoad;
     public Vector2 playerPosition;
     public VectorValue playerLocation;
+    public GameObject fadePanel;
+
+    private void Awake()
+    {
+        if(fadePanel != null)
+        {
+            GameObject panel = Instantiate(fadePanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+    }
     public void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player") && !other.isTrigger)
--- Assets/Scripts/ScriptableObjects/VectorValue.cs
+++ Assets/Scripts/ScriptableObjects/VectorValue.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class VectorValue : ScriptableObject
+public class VectorValue : ScriptableObject, ISerializationCallbackReceiver
 {
     public Vector2 initialValue;
     // Start is called before the first frame update
+    public Vector2 defaultValue;
+    public void OnAfterDeserialize()
+    {
+        initialValue = defaultValue;
+
+    }
+
+    public void OnBeforeSerialize()
+    {
+
+    }
  
 }
